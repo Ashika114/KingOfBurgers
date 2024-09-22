90ccda049853ce0ac4d414c485e5499b3ce1c5c0
Ashika Patel <patel.ashika4100@gmail.com>
2024-09-22 06:53:28 -0700

    Update burgerorderingsystem.sql

--- burgerorderingsystem.sql
+++ burgerorderingsystem.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Sep 22, 2024 at 05:15 AM
--- Server version: 5.7.36
--- PHP Version: 7.4.26
+-- Host: 127.0.0.1
+-- Generation Time: Sep 22, 2024 at 03:52 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,28 +27,22 @@ SET time_zone = "+00:00";
 -- Table structure for table `fooditems`
 --
 
-DROP TABLE IF EXISTS `fooditems`;
-CREATE TABLE IF NOT EXISTS `fooditems` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `fooditems` (
+  `id` int(11) NOT NULL,
   `itemName` varchar(255) NOT NULL,
   `itemType` varchar(10) NOT NULL,
   `price` int(11) NOT NULL,
   `discountedPrice` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
+  `filePath` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `fooditems`
 --
 
-INSERT INTO `fooditems` (`id`, `itemName`, `itemType`, `price`, `discountedPrice`) VALUES
-(1, 'Whopper', 'Veg', 150, 120),
-(2, 'Whopper', 'Non-Veg', 180, 0),
-(3, 'Whopper Jr.', 'Veg', 100, 0),
-(4, 'Whopper Jr.', 'Non-Veg', 120, 0),
-(5, 'King', 'Veg', 180, 10),
-(6, 'King', 'Non-Veg', 200, 0),
-(13, 'Paneer Burger', 'Veg', 300, 0);
+INSERT INTO `fooditems` (`id`, `itemName`, `itemType`, `price`, `discountedPrice`, `filePath`) VALUES
+(16, 'Whopper', 'Veg', 200, 0, 'uploads/Whopper.png'),
+(17, 'Whopper', 'Non-Veg', 400, 0, 'uploads/Whopper Jr.png');
 
 -- --------------------------------------------------------
 
@@ -56,20 +50,15 @@ INSERT INTO `fooditems` (`id`, `itemName`, `itemType`, `price`, `discountedPrice
 -- Table structure for table `loginform`
 --
 
-DROP TABLE IF EXISTS `loginform`;
-CREATE TABLE IF NOT EXISTS `loginform` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `loginform` (
+  `id` int(11) NOT NULL,
   `fullname` varchar(50) NOT NULL,
   `username` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `admin` varchar(3) NOT NULL DEFAULT 'NO',
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `email` (`email`),
-  KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `loginform`
@@ -85,9 +74,8 @@ INSERT INTO `loginform` (`id`, `fullname`, `username`, `email`, `password`, `adm
 -- Table structure for table `orders`
 --
 
-DROP TABLE IF EXISTS `orders`;
-CREATE TABLE IF NOT EXISTS `orders` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
   `fullname` varchar(50) NOT NULL,
   `username` varchar(50) NOT NULL,
   `order_id` varchar(50) NOT NULL,
@@ -96,18 +84,65 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `email` varchar(50) NOT NULL,
   `address` varchar(255) NOT NULL,
   `details` varchar(50) NOT NULL DEFAULT 'Confirmed',
-  `order_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `price` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `order_id` (`order_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+  `order_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
 
 --
 -- Dumping data for table `orders`
 --
 
 INSERT INTO `orders` (`id`, `fullname`, `username`, `order_id`, `item`, `quantity`, `email`, `address`, `details`, `order_time`, `price`) VALUES
-(1, 'dev Patel', 'dev123', '21092024035252', 'Whopper Non-Veg, Whopper Veg, ', '10, 1, ', 'dev@gmail.com', '123 Patel Street Navsari', 'Delivered', '2024-09-21 21:22:52', 2106);
+(1, 'dev Patel', 'dev123', '21092024035252', 'Whopper Non-Veg, Whopper Veg, ', '10, 1, ', 'dev@gmail.com', '123 Patel Street Navsari', 'Delivered', '2024-09-21 21:22:52', 2106),
+(2, 'dev', 'dev123', '22092024123918', 'Whopper, ', '1, ', 'patel.ashika28@yahoo.in', '123 Patel Street Navsari', 'Confirmed', '2024-09-22 03:39:18', 216);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `fooditems`
+--
+ALTER TABLE `fooditems`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `loginform`
+--
+ALTER TABLE `loginform`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `order_id` (`order_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `fooditems`
+--
+ALTER TABLE `fooditems`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `loginform`
+--
+ALTER TABLE `loginform`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
